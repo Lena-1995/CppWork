066942c9f1e7fb309be59d5832b9feb97a906bcc
avivbron1994 <avivbron1994@gmail.com>
2019-12-22 16:28:48 +0200

    implement Employee
    implement Employee

--- Project1/Project1/Employee.cpp
+++ Project1/Project1/Employee.cpp
@@ -4,9 +4,18 @@
 
 Employee::Employee()
 {
+	cout << "Enter the salary of the employed person" << endl;
+	cin << Salary; 
 }
 
 
 Employee::~Employee()
 {
+}
+
+void Employee::Print()
+{
+	Person::Print();
+	cout << "The salary of the Employee is : " << Salary << endl;
+
 }
--- Project1/Project1/Employee.h
+++ Project1/Project1/Employee.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Person.h"
-class Employee :
-	public Person
+class Employee : public Person
 {
+private:
+	float Salary;
+
 public:
 	Employee();
 	~Employee();
+	void Print();
+	
+	
 };
 
